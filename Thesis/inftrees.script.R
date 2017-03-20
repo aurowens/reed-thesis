061b9f303806b1bcd777d8fcec6a1a8eea033d18
aurowens <aurowens@reed.edu>
2017-03-20 13:27:17 -0700

    fixed a few plots in ch4

--- Thesis/inftrees.script.R
+++ Thesis/inftrees.script.R
@@ -1,28 +1,74 @@
 ################INFTREES############
-
-partition.grabber <- function(vars, sp){
-  p <- rep("", 1:dim(splits)[1])
-  for (i in 1:dim(sp)[1]) {p[i] <- sp[[i]]}
-  p[p == ""] <- NA
-  p <- as.double(p)
-  return(data.frame("cutleft" = p, "vars" = vars))
+intervalReturn <- function(split.cutleft){ 
+  intervals <- data.frame(split.cutleft)
+  intervals <- filter(intervals, split.cutleft != "")
+  intervals <- dplyr::select(intervals, split.cutleft)
+  intervals$split.cutleft <- gsub("<", "", intervals$split.cutleft)
+  intervals$split.cutleft <- as.numeric(intervals$split.cutleft)
+  return(arrange(intervals, (split.cutleft)))
 }
 
-##start w/tree?
-inftree <- function(t, x, y) {
-  rss <- predict(t, x, y)
-  vi <- rep(0, ncol(x))
-  for(i in 1:(ncol(d)-1)) {
-    x1 <- x
-    ti <- tree(y = x[,i], x= x[,-1], data = x)
-    p <- partition.grabber(as.character(ti$frame$var), ti$frame$splits) #p is df with vars and their splits
-    for( j in nrow(p)) {
-      x1[p[i],i] <- sample(x[p[i],i])
-    }
-    vi[i] <- rss - predict(ti, x1, y)
+partitionCreator <- function(i = interval, x, data) {
+  p <- c(1:(1+nrow(i)))
+  bins <- data.frame (bin = rep(1, nrow(data)), v = data[,x])
+  for(j in 1:nrow(i)){
+    bins[bins$v > i[j,1],1] <- p[j+ 1]
   }
-  return(vi)
+  return(bins$bin)
 }
 
-data("mtcars")
-terr <- tree(mpg~., data = mtcars)
+condPermuter <- function(d2, x) {
+  d2$p <- as.factor(d2$p)
+  for(i in 1:length(levels(d2$p))){
+    #set.seed(1)
+    d2[d2$p == i, x] <- sample(d2[d2$p== i,x])
+  }
+  return(d2[,x])
+}
+
+VIdevBagged<- function(b, x){
+  return(as.numeric(as.data.frame(b$mtrees[[1]]$btree$frame)[,c(1,4)] %>% filter(var == x) %>% dplyr::summarise(sum = sum(dev))))
+}
+
+VIdev<- function(t2, x){
+  return(as.data.frame(t2$frame)[,c(1,3)] %>% 
+           filter(var == x) %>% dplyr::summarise(sum = sum(dev)))
+}
+
+variable.importance.dev <- function(t1, x, d){
+  if(length(t1$frame$splits[,1]) == 1) {
+    return(VIdev(t1, x))
+  } else {
+    interval <- intervalReturn(t1$frame$splits[,1])
+    d$p <- partitionCreator(interval, x, d)
+    d[,x] <- condPermuter(d, x)
+    t2 <- tree(y ~ ., data = d, y = TRUE)
+    v1 <- VIdev(t2, x)
+    #rm(t1, t2)
+    return(v1)
+  }
+}
+
+cond.varImp <- function(xs, data) {
+  x <- names(xs)
+  vi <- rep(0,length(x))
+  vo <- rep(0, length(x))
+  
+  t0 <- tree(y~., data)
+  
+  set.seed(1)
+  for(i in 1:length(x)){
+    vo[i] <- VIdev(t0, x[i])
+  }
+  
+  set.seed(1)
+  for(i in 1:length(x)){
+    form <- as.formula(paste(x[i], "~."))
+    ti <- tree(form, xs)
+    vi[i] <- variable.importance.dev(ti, x[i], data)
+  }
+  
+  v <- data.frame("variable" = x, "permuted_variable_importance" = as.numeric(vi), "base_variable_importance"= as.numeric(vo))
+  return(v)
+}
+
