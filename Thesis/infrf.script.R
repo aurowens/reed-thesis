4e70f21b5e244ab70e3ba705e6e2a0f5482045fa
aurowens <aurowens@reed.edu>
2017-03-20 17:29:02 -0700

    game plan for this week

---
+++ Thesis/infrf.script.R
@@ -0,0 +1,13 @@
+
+
+#########################TO DO:
+  
+#####  Imma have to write my own random forest code, y'all 
+rf <- function(form, data) { 
+  #imputs: formula to be tested and the dataset to test on. outputs: tribble,
+  #contains: total oob error, every tree
+}
+
+#then infrf is simply calls to inftrees, repeated. Produces a p-val from distribution of values from the rf
+
+
